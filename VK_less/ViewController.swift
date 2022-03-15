afe84bd885f5fcd88ac6f389201296992ff1c298
Rybacheque <Fishchek@gmail.com>
2022-03-15 13:43:26 +0300

    Initial Commit

---
+++ VK_less/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VK_less
+//
+//  Created by Сергей Рыбачек on 15.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
